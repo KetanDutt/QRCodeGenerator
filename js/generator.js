dba1d5ec610589d13ab17f091aef9bf573f79052
Ketan Dutt <ketan.dutt@yahoo.in>
2024-02-02 13:44:04 +0530

    Update generator.js

--- js/generator.js
+++ js/generator.js
@@ -9,7 +9,7 @@ function download_image(url, id) {
 }
 
 function VcardURL(contact) {
-    url = "https://quickchart.io/qr?text=BEGIN%3AVCARD%0AVERSION%3A3.0%0AN%3A" + contact["Last Name"] + "%3B" + contact["First Name"] +
+    url = "https://quickchart.io/qr?size=1000&text=BEGIN%3AVCARD%0AVERSION%3A3.0%0AN%3A" + contact["Last Name"] + "%3B" + contact["First Name"] +
         "%0AN%3A" + contact["First Name"] + "%20" + contact["Last Name"] + "%0AORG%3A" + "RTC Tek" + "%0ATITLE%3A" + contact["Designation"] +
         "%0AADR%3A%3B%3B" + "" + "%3B" + "" + "%3B" + "" + "%3B" + "" + "%3B" + "India" + "%0ATEL%3BWORK%3BVOICE%3A" + contact["Employee Contact Number"] +
         "%0ATEL%3BCELL%3A" + contact["Emergency Contact Number (Should not be same as employee contact number)"] + "%0ATEL%3BFAX%3A%0AEMAIL%3BWORK%3BINTERNET%3A" + contact["Official Email ID"] + "%0AURL%3A" + "" + "%0AEND%3AVCARD"
